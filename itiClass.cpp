864b5ad70bf05ffd5e7afc17d45b0868564e75d9
Elkhaligy Laptop <shehabelkhaligy100@gmail.com>
2025-01-05 11:41:03 +0200

    Added: itiClass.cpp test.txt worldClass.cpp

---
+++ itiClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class HelloITI
+{
+    public:
+    void printHelloITI()
+    {
+        cout << "Hello ITI";
+    }
+};
+
+int main()
+{
+    return 0;
+}
---
+++ worldClass.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class HelloWorld
+{
+    public:
+    void printHelloWorld()
+    {
+        cout << "Hello World";
+    }
+};
+
+int main()
+{
+    return 0;
+}
